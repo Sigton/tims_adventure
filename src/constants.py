b9f039c8bed00f5879d15d567e256c1625b1b391
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-07-28 22:13:14 +0100

    Updated constants.py

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+"""
+Constants.py
+
+This file defines variables
+that will remain unchanged
+throughout the entirety of
+the program.
+"""
+
+DISPLAY_WIDTH = 960
+DISPLAY_HEIGHT = 720
+DISPLAY_SIZE = (DISPLAY_WIDTH, DISPLAY_HEIGHT)
