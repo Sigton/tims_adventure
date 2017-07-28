026d3bbda375ed03075958482b05a854e8eba8f0
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-07-28 22:01:51 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+import cx_Freeze
+
+executables = [cx_Freeze.Executable(script="src/main.py",
+                                    targetName="BeanRPG.exe")]
+
+include_files = ["src"]
+
+packages = ["pygame",
+            "os"]
+
+excludes = ["tkinter",
+            "numpy",
+            "OpenGL",
+            "html",
+            "http",
+            "email",
+            "multiprocessing",
+            "urllib",
+            "xml",
+            "socket"]
+
+cx_Freeze.setup(
+    name="Bean RPG",
+    options={
+        "build_exe": {
+            "packages": packages,
+            "excludes": excludes,
+            "include_files": include_files
+        }
+    },
+    executables=executables
+)
---
+++ src/main.py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.locals import *
+
+
+class Main:
+
+    def __init__(self):
+
+        pygame.init()
+
+if __name__ == "__main__":
+    game = Main()
