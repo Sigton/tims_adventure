2e4b312270ce862d6cd4872cdec4c4b76ddfa2a1
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-07-28 23:04:57 +0100

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
 import pygame
 from pygame.locals import *
 
+from src import constants
+
 
 class Main:
 
     def __init__(self):
 
+        # Initiate pygame
         pygame.mixer.pre_init(22050, -16, 1, 512)
         pygame.mixer.init()
         pygame.init()
+
+        # Create the display
+        pygame.display.set_mode(constants.DISPLAY_SIZE)
+
+        # Set the title on the window
+        pygame.display.set_caption("Bean RPG")
 
 if __name__ == "__main__":
     game = Main()
