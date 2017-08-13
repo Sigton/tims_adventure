832f065f1dd43e7c3c1714f630a5b1f06dee39ff
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-08-13 22:17:14 +0100

    Added get_image method

--- src/spritesheet.py
+++ src/spritesheet.py
@@ -18,3 +18,17 @@ class SpriteSheet(object):
 
         # Load the sprite sheet
         self.sprite_sheet = pygame.image.load(filename).convert()
+
+    def get_image(self, x, y, width, height):
+
+        # Create a blank image
+        image = pygame.Surface([width, height]).convert()
+
+        # Copy the sprite from the sprite sheet
+        image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
+
+        # Set white to transparent
+        image.set_colorkey(constants.WHITE)
+
+        # Return the image
+        return image
