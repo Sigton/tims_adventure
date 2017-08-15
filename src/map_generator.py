a3e4552baa30a299aac635042a8cbaae0de12249
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-08-15 18:43:17 +0100

    Updated map generator

--- src/map_generator.py
+++ src/map_generator.py
@@ -1,5 +1,9 @@
 import pygame
 
+import operator
+
+from src import constants
+
 """
 map_generator.py
 
@@ -13,4 +17,28 @@ def generate_map(blueprint):
     # This function takes a blueprint diagram of a world map
     # and turns it into a format use-able by the terrain engine.
 
-    map_img = pygame.image.load("resources/map.png")
+    map_img = pygame.image.load(blueprint)
+    print("Image loaded.")
+
+    map_pix_arr = pygame.PixelArray(map_img)
+    print("Created pixel array with shape {}.".format(map_pix_arr.shape))
+
+    chunks_wide = tuple(map(operator.floordiv, map_pix_arr.shape,
+                           (constants.chunk_w, constants.chunk_h)))
+    num_chunks = chunks_wide[0] * chunks_wide[1]
+    print("Preparing to arrange {} chunks.".format(num_chunks))
+
+    chunk_x, chunk_y = 0, 0
+    for n in range(num_chunks):
+
+        chunk_x += 1
+        if chunk_x % chunks_wide[0] == 0:
+            chunk_x = 0
+            chunk_y += 1
+
+
+if __name__ == '__main__':
+
+    pygame.init()
+
+    generate_map("resources/map.png")
