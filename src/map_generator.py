4b6003480b7e691bfa1db27f97c4ced10dfc70f9
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-08-15 17:46:34 +0100

    Created generate_map function

--- src/map_generator.py
+++ src/map_generator.py
@@ -6,3 +6,11 @@ map_generator.py
 Turns a 2000x1500 image into
 JSON data.
 """
+
+
+def generate_map(blueprint):
+
+    # This function takes a blueprint diagram of a world map
+    # and turns it into a format use-able by the terrain engine.
+
+    pass
