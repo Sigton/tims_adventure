bc15fd56c1f7f84e079422fd3601e3f782701cde
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-08-15 15:30:33 +0100

    Added more tiles

--- src/tile_image_loader.py
+++ src/tile_image_loader.py
@@ -12,8 +12,27 @@ so they can be quickly accessed to improve performance.
 
 generic_ground = (0, 0, 48, 48)
 water = (48, 0, 48, 48)
+path_1 = (0, 48, 48, 48)
+path_2 = (48, 48, 48, 48)
+path_3 = (96, 48, 48, 48)
+path_4 = (144, 48, 48, 48)
+path_5 = (192, 48, 48, 48)
+path_6 = (240, 48, 48, 48)
+path_7 = (288, 48, 48, 48)
+path_8 = (336, 48, 48, 48)
+path_9 = (0, 96, 48, 48)
+path_10 = (48, 96, 48, 48)
+path_11 = (96, 96, 48, 48)
+path_12 = (144, 96, 48, 48)
+path_13 = (192, 96, 48, 48)
+path_14 = (240, 96, 48, 48)
+path_15 = (288, 96, 48, 48)
 
-tiles = [generic_ground, water]
+tiles = [generic_ground, water, path_1,
+         path_2, path_3, path_4, path_5,
+         path_6, path_7, path_8, path_9,
+         path_10, path_11, path_12,
+         path_13, path_14, path_15]
 images = []
 
 sprite_sheet = None
