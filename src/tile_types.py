348eb6ba3899f3787f596545bb209af5c496f3a6
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-08-20 12:03:43 +0100

    Defined solids

--- src/tile_types.py
+++ src/tile_types.py
@@ -5,3 +5,15 @@ This defines how the player
 interacts with the tiles in
 the game.
 """
+
+solid_tiles = [
+    "0017",
+    "0018",
+    "0019",
+    "0020",
+    "0021",
+    "0022",
+    "0023",
+    "0024",
+    "0025"
+]
