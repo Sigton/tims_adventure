678383e7e97ef48579d81737838cc783acc0e1bf
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-08-23 20:26:19 +0100

    Defined load_sprite_sheet

--- src/bean_image_loader.py
+++ src/bean_image_loader.py
@@ -7,3 +7,10 @@ This file loads all
 the bean images
 """
 
+sprite_sheet = None
+
+
+def load_sprite_sheet():
+
+    global sprite_sheet
+    sprite_sheet = pygame.image.load("src/resources/beans.png")
