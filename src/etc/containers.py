b088d363278c055c2787aa7db016171dbac52508
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-09-06 07:23:35 +0100

    Created Seed object

--- src/etc/containers.py
+++ src/etc/containers.py
@@ -4,3 +4,9 @@ containers.py
 Here I create my own container classes
 that run more efficiently than python's dicts
 """
+
+
+class Seed(object):
+
+    # Using __slots__ to maximise memory efficiency
+    __slots__ = ["tiles", "decs", "entities"]
