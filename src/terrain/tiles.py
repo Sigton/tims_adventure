d69e985f1e527f4bed7b2c24529a5c0dba8f871a
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-09-06 16:52:16 +0100

    Updated AnimatedTile.reuse

--- src/terrain/tiles.py
+++ src/terrain/tiles.py
@@ -91,6 +91,20 @@ class AnimatedTile(pygame.sprite.Sprite):
             self.current_image = (self.current_image + 1) % len(self.images)
             self.image = self.images[self.current_image]
 
-    def reuse(self):
+    def reuse(self, tile_images_index, x, y, o_x, o_y, tile_code):
 
-        pass
+        if tile_code not in animated_tiles:
+            new_tile = Tile(tile_images_index, x, y, o_x, o_y, tile_code)
+            [group.add(new_tile) for group in self.groups()]
+        else:
+            self.tile_code = tile_code
+
+            self.images = [tile for tile in images[tile_images_index]]
+            self.image = self.images[0]
+
+            self.rect = self.image.get_rect()
+            self.rect.x = x * constants.tile_w
+            self.rect.y = y * constants.tile_h
+
+            self.offset_x = o_x
+            self.offset_y = o_y
