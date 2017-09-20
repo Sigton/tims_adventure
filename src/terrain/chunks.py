f8422c9ffa849b5e48b285b226d4aa3e4450fc3e
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-09-20 18:52:22 +0100

    Added support for animated decorations

--- src/terrain/chunks.py
+++ src/terrain/chunks.py
@@ -241,9 +241,16 @@ class ChunkController:
             tile = dec["tileid"]
 
             if len(position) == 4:
-                new_chunk.add_dec(tiles.Tile(int(tile), int(position[0:2]), int(position[2:4]), tile, True))
+                x, y = int(position[0:2]), int(position[2:4])
+                to_grid = True
             else:
-                new_chunk.add_dec(tiles.Tile(int(tile), int(position[0:3]), int(position[3:6]), tile, False))
+                x, y = int(position[0:3]), int(position[3:6]),
+                to_grid = False
+
+            if tile in animated_tiles:
+                new_chunk.add_dec(tiles.AnimatedTile(int(tile), x, y, tile, to_grid))
+            else:
+                new_chunk.add_dec(tiles.Tile(int(tile), x, y, tile, to_grid))
 
         # Add them to the dict of tiles
         self.map_tiles[chunk] = new_chunk
