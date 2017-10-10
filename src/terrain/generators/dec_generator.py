5f430426b690ac6348031796a49c194df20eaa4e
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-10-10 19:57:36 +0100

    Updated dec_generator

--- src/terrain/generators/dec_generator.py
+++ src/terrain/generators/dec_generator.py
@@ -1,5 +1,8 @@
 import pygame
-import os
+
+from src.etc import constants
+
+import os, json, operator
 
 """
 dec_generator.py
@@ -17,6 +20,12 @@ def generate_decs(map_dir):
     files = os.listdir(map_dir)
 
     pix_arrays = [pygame.PixelArray(pygame.image.load(file)) for file in files]
+
+    for layer in pix_arrays:
+
+        chunks_wide = tuple(map(operator.floordiv, layer.shape,
+                                (constants.chunk_w, constants.chunk_h)))
+        num_chunks = chunks_wide[0] * chunks_wide[1]
 
 
 if __name__ == "__main__":
