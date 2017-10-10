e1c52e8c6b6ef6a3c38df35033d3b8a07624f411
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-10-10 16:40:17 +0100

    Added doc string

---
+++ src/terrain/generators/dec_generator.py
@@ -0,0 +1,9 @@
+"""
+dec_generator.py
+
+This file translates external decoration
+maps into JSON format. The decoration
+maps are not included in source to save
+storage space, so this should never be ran
+in runtime.
+"""
