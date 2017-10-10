b1a8c4c12b52e600e49ee7223ff711b096f9bd41
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-10-10 20:43:06 +0100

    Chunk data now gets separated into individual chunks

--- src/terrain/generators/dec_generator.py
+++ src/terrain/generators/dec_generator.py
@@ -1,6 +1,7 @@
 import pygame
 
 from src.etc import constants
+from src.terrain import tile_data
 
 import os, json, operator
 
@@ -26,6 +27,21 @@ def generate_decs(map_dir):
         chunks_wide = tuple(map(operator.floordiv, layer.shape,
                                 (constants.chunk_w, constants.chunk_h)))
         num_chunks = chunks_wide[0] * chunks_wide[1]
+
+        chunks = []
+        chunk_x, chunk_y = 0, 0
+        for n in range(num_chunks):
+
+            chunks += [[[layer[x + (chunk_x * constants.chunk_w)][y + (chunk_y * constants.chunk_h)]
+                         for x in range(constants.chunk_w)]
+                        for y in range(constants.chunk_h)]]
+
+            chunk_x += 1
+            if chunk_x % chunks_wide[0] == 0:
+                chunk_x = 0
+                chunk_y += 1
+
+        chunks = [sum(chunk, []) for chunk in chunks]
 
 
 if __name__ == "__main__":
