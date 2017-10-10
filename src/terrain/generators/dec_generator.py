c0a61510035b304808875924188bae1ce9c61380
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-10-10 16:48:22 +0100

    Updated dec_generator

--- src/terrain/generators/dec_generator.py
+++ src/terrain/generators/dec_generator.py
@@ -7,3 +7,12 @@ maps are not included in source to save
 storage space, so this should never be ran
 in runtime.
 """
+
+
+def generate_decs(map_dir):
+
+    pass
+
+
+if __name__ == "__main__":
+    generate_decs("D:/bean_rpg_etc/decmaps")
