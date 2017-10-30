890fa80be31cddd3ebb6d915e4fb52b46ca36d3f
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-10-30 21:05:04 +0000

    Defined SoundEngine

--- src/sounds.py
+++ src/sounds.py
@@ -3,3 +3,10 @@ import pygame
 # This deals with the loading and playing of sounds.
 # Each sound has it's own channel so that we don't
 # run out of channels.
+
+
+class SoundEngine:
+
+    def __init__(self):
+
+        self.ambient1_channel = pygame.mixer.Channel(0)
