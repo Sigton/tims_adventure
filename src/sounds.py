b3ca3522b3fcdbdbe555176ac2b574d553d1cc1d
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-10-30 21:06:51 +0000

    Added music to the sound engine

--- src/sounds.py
+++ src/sounds.py
@@ -9,4 +9,8 @@ class SoundEngine:
 
     def __init__(self):
 
+        # Create a channel for each sound
         self.ambient1_channel = pygame.mixer.Channel(0)
+
+        # Load all the sounds
+        self.ambient1_sound = pygame.mixer.Sound("src/resources/ambient1.mp3")
