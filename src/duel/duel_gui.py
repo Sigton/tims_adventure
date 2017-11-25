78094c8f20aa3439854e78232772d642047b9701
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-11-25 11:54:08 +0000

    Defined load_images in duel_gui

--- src/duel/duel_gui.py
+++ src/duel/duel_gui.py
@@ -1,5 +1,7 @@
 import pygame
 
+from src.etc import spritesheet
+
 """
 duel_gui.py
 
@@ -32,3 +34,39 @@ class SpriteSheetData:
         (0, 583, 202, 41),
         (202, 583, 202, 41)
     ]
+
+
+def load_images():
+
+    sprite_sheet = spritesheet.SpriteSheet("src/resources/duel_gui.png")
+
+    images = {
+        "player_back_plate": sprite_sheet.get_image(SpriteSheetData.player_back_plate[0],
+                                                    SpriteSheetData.player_back_plate[1],
+                                                    SpriteSheetData.player_back_plate[2],
+                                                    SpriteSheetData.player_back_plate[3]
+                                                    ),
+        "enemy_back_plate": sprite_sheet.get_image(SpriteSheetData.enemy_back_plate[0],
+                                                   SpriteSheetData.enemy_back_plate[1],
+                                                   SpriteSheetData.enemy_back_plate[2],
+                                                   SpriteSheetData.enemy_back_plate[3]),
+        "attack_main_button": [sprite_sheet.get_image(image[0],
+                                                      image[1],
+                                                      image[2],
+                                                      image[3]) for image in SpriteSheetData.attack_main_button],
+        "attack_alt_button": [sprite_sheet.get_image(image[0],
+                                                     image[1],
+                                                     image[2],
+                                                     image[3]) for image in SpriteSheetData.attack_main_button],
+        "item_button": [sprite_sheet.get_image(image[0],
+                                               image[1],
+                                               image[2],
+                                               image[3]) for image in SpriteSheetData.attack_main_button],
+        "retreat_button": [sprite_sheet.get_image(image[0],
+                                                  image[1],
+                                                  image[2],
+                                                  image[3]) for image in SpriteSheetData.attack_main_button],
+        "background": pygame.image.load("src/resources/duel_background.png").convert()
+    }
+
+    return images
