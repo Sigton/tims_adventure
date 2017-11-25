651551950933df7c068492090504e3a047c1a779
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-11-25 12:39:36 +0000

    Created button class in gui_components

---
+++ src/etc/gui_components.py
@@ -0,0 +1,24 @@
+"""
+gui_components.py
+
+This file holds generic items
+found on most gui's
+"""
+
+
+class Button:
+
+    def __init__(self, images, x, y):
+
+        self.inactive_image = images[0]
+        self.active_image = images[1]
+
+        self.image = self.inactive_image
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def draw(self, display):
+
+        display.blit(self, (self.rect.x, self.rect.y))
