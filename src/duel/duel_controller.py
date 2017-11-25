2c70df6df5f665b26a83620d8ec3cd4513e457f8
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-11-25 12:42:29 +0000

    Implemented the buttons

--- src/duel/duel_controller.py
+++ src/duel/duel_controller.py
@@ -1,4 +1,5 @@
 from src.duel.duel_gui import load_images
+from src.etc import gui_components
 
 """
 duel.py
@@ -15,6 +16,18 @@ class DuelController:
 
         self.background = self.images["background"]
 
+        self.attack_main_button = gui_components.Button(self.images["attack_main_button"], 0, 0)
+        self.attack_alt_button = gui_components.Button(self.images["attack_alt_button"], 0, 50)
+        self.item_button = gui_components.Button(self.images["item_button"], 0, 100)
+        self.retreat_button = gui_components.Button(self.images["retreat_button"], 0, 150)
+
+        self.buttons = [
+            self.attack_main_button,
+            self.attack_alt_button,
+            self.item_button,
+            self.retreat_button
+        ]
+
     def update(self):
 
         pass
@@ -22,3 +35,5 @@ class DuelController:
     def draw(self, display):
 
         display.blit(self.background, (0, 0))
+
+        [button.draw(display) for button in self.buttons]
