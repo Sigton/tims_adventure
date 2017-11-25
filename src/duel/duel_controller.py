02a156de0ef9394de5ad4a57309ed89427fe9c78
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-11-25 10:49:15 +0000

    Created duel_gui.py

--- src/duel/duel_controller.py
+++ src/duel/duel_controller.py
@@ -4,6 +4,7 @@ duel.py
 This deals with the game mechanics and GUI of duelling
 """
 
+
 class DuelController:
 
     def __init__(self):
---
+++ src/duel/duel_gui.py
@@ -0,0 +1,6 @@
+"""
+duel_gui.py
+
+This loads the components of the
+user interface for the duel scenes
+"""
