a68a5a248fba7d8b15dffd8819e6be4df4089a60
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-11-25 11:23:53 +0000

    Added encased spritesheet data in a class

--- src/duel/duel_gui.py
+++ src/duel/duel_gui.py
@@ -8,25 +8,27 @@ user interface for the duel scenes
 """
 
 
-enemy_back_plate = (0, 0, 480, 150)
-player_back_plate = (0, 150, 480, 310)
+class SpriteSheetData:
 
-attack_main_button = [
-    (0, 460, 202, 41),
-    (202, 460, 202, 41)
-]
+    enemy_back_plate = (0, 0, 480, 150)
+    player_back_plate = (0, 150, 480, 310)
 
-attack_alt_button = [
-    (0, 501, 202, 41),
-    (202, 501, 202, 41)
-]
+    attack_main_button = [
+        (0, 460, 202, 41),
+        (202, 460, 202, 41)
+    ]
 
-item_button = [
-    (0, 542, 202, 41),
-    (202, 542, 202, 41)
-]
+    attack_alt_button = [
+        (0, 501, 202, 41),
+        (202, 501, 202, 41)
+    ]
 
-retreat_button = [
-    (0, 583, 202, 41),
-    (202, 583, 202, 41)
-]
+    item_button = [
+        (0, 542, 202, 41),
+        (202, 542, 202, 41)
+    ]
+
+    retreat_button = [
+        (0, 583, 202, 41),
+        (202, 583, 202, 41)
+    ]
