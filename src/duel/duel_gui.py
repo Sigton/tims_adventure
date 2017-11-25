60ce8c4c1c481d95fa1884c1078cd055df05eb31
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-11-25 11:22:44 +0000

    Added spritesheet data

--- src/duel/duel_gui.py
+++ src/duel/duel_gui.py
@@ -1,6 +1,32 @@
+import pygame
+
 """
 duel_gui.py
 
 This loads the components of the
 user interface for the duel scenes
 """
+
+
+enemy_back_plate = (0, 0, 480, 150)
+player_back_plate = (0, 150, 480, 310)
+
+attack_main_button = [
+    (0, 460, 202, 41),
+    (202, 460, 202, 41)
+]
+
+attack_alt_button = [
+    (0, 501, 202, 41),
+    (202, 501, 202, 41)
+]
+
+item_button = [
+    (0, 542, 202, 41),
+    (202, 542, 202, 41)
+]
+
+retreat_button = [
+    (0, 583, 202, 41),
+    (202, 583, 202, 41)
+]
