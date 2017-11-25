b42f0aa717c212913943a342c147886752c4dc4f
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-11-25 10:03:18 +0000

    Created entities.py

---
+++ src/entities.py
@@ -0,0 +1,13 @@
+"""
+entities.py
+
+This file holds the classes for on screen entities,
+and metadata about entities
+"""
+
+
+class EntityMeta:
+
+    def __init__(self, parent):
+
+        self.parent = parent
