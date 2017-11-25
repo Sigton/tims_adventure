03699cee4ae598219219711ccd204d43f1304f4f
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-11-25 11:59:59 +0000

    Renaming things

--- src/duel/duel_gui.py
+++ src/duel/duel_gui.py
@@ -12,9 +12,6 @@ user interface for the duel scenes
 
 class SpriteSheetData:
 
-    enemy_back_plate = (0, 0, 480, 150)
-    player_back_plate = (0, 150, 480, 310)
-
     attack_main_button = [
         (0, 460, 202, 41),
         (202, 460, 202, 41)
@@ -38,18 +35,9 @@ class SpriteSheetData:
 
 def load_images():
 
-    sprite_sheet = spritesheet.SpriteSheet("src/resources/duel_gui.png")
+    sprite_sheet = spritesheet.SpriteSheet("src/resources/duel_gui_buttons.png")
 
     images = {
-        "player_back_plate": sprite_sheet.get_image(SpriteSheetData.player_back_plate[0],
-                                                    SpriteSheetData.player_back_plate[1],
-                                                    SpriteSheetData.player_back_plate[2],
-                                                    SpriteSheetData.player_back_plate[3]
-                                                    ),
-        "enemy_back_plate": sprite_sheet.get_image(SpriteSheetData.enemy_back_plate[0],
-                                                   SpriteSheetData.enemy_back_plate[1],
-                                                   SpriteSheetData.enemy_back_plate[2],
-                                                   SpriteSheetData.enemy_back_plate[3]),
         "attack_main_button": [sprite_sheet.get_image(image[0],
                                                       image[1],
                                                       image[2],
@@ -66,7 +54,7 @@ def load_images():
                                                   image[1],
                                                   image[2],
                                                   image[3]) for image in SpriteSheetData.attack_main_button],
-        "background": pygame.image.load("src/resources/duel_background.png").convert()
+        "background": pygame.image.load("src/resources/duel_gui.png").convert()
     }
 
     return images
