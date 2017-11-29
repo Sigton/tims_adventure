3d50809666069e8a2279b22ace0f7ca539b29c37
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-11-29 22:11:59 +0000

    Added particles when the player gets hit

--- src/duel/duel_controller.py
+++ src/duel/duel_controller.py
@@ -122,6 +122,8 @@ class DuelController:
             self.turn = 0
             self.turn_cool_down = 50
 
+            self.particle_engine.create_particle_spread("fire", 40, 220, 530, 180, 160, 25, 20, 25, 5)
+
         self.update_gui_components()
 
     def callback(self, button_id):
@@ -139,7 +141,7 @@ class DuelController:
                 self.turn = 1
                 self.turn_cool_down = 50
 
-                self.particle_engine.create_particle_spread("fire", 30, 750, 170, 140, 120, 25, 20, 25, 10)
+                self.particle_engine.create_particle_spread("fire", 30, 750, 170, 140, 120, 25, 20, 25, 5)
 
     def update_gui_components(self):
 
