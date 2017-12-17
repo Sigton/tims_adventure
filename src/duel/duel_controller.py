dab6b3d975f10624c5390926e312fec18064ce5b
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-12-17 12:13:24 +0000

    enemy is now using moves from moves file

--- src/duel/duel_controller.py
+++ src/duel/duel_controller.py
@@ -140,15 +140,18 @@ class DuelController:
             self.turn_cool_down -= 1
 
         if self.turn == 1 and not self.turn_cool_down:
-            self.player.hp -= self.enemy.attack
+            self.player.hp -= int(self.enemy.attack * moves[self.player.moves[0]]["str_mod"])
             if self.player.hp < 0:
                 self.player.hp = 0
 
             self.turn = 0
             self.turn_cool_down = 150
 
-            self.particle_engine.create_particle_spread("fire", 40, 220, 530, 170, 25, 20, 25, 5)
-            self.start_shake_enemy(18, 10, 10, -1)
+            move = moves[self.player.moves[0]]["effects"]
+
+            if moves[self.player.moves[0]]["name"] in constants.shake_moves:
+                move = move.format("enemy", "-")
+            exec(move)
 
         self.shake_players()
 
