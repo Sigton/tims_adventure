b82895a8849782d34a55a6824be7e8e9a7599946
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-12-17 14:47:34 +0000

    Added energy attribute to moves

--- src/duel/moves.py
+++ src/duel/moves.py
@@ -8,7 +8,10 @@ a player can make.
 """
 
 moves = [
-    {"name": "Tackle", "str_mod": 1, "effects": "self.start_shake_{}(18, 10, 10, {}1)"},
+    {"name": "Tackle", "str_mod": 1, "effects":
+        "self.start_shake_{}(18, 10, 10, {}1)",
+     "energy": 2},
     {"name": "Burn", "str_mod": 1.4, "effects":
-        """self.particle_engine.create_particle_spread('fire', 30, {}, {}, 130, 25, 20, 25, 5)"""}
+        """self.particle_engine.create_particle_spread('fire', 30, {}, {}, 130, 25, 20, 25, 5)""",
+     "energy": 5}
 ]
