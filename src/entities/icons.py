c3be60d289053fdb75197327d0fb0b4537eda037
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-12-19 16:38:32 +0000

    Added drawing and positioning of Icon

--- src/entities/icons.py
+++ src/entities/icons.py
@@ -8,10 +8,13 @@ small icons that appear about the place
 
 class Icon:
 
-    def __init__(self, image, flash=False, flash_threshold=0):
+    def __init__(self, image, x, y, flash=False, flash_threshold=0):
 
         self.image = image
+
         self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
         self.flash = flash
         self.flash_threshold = flash_threshold
@@ -26,3 +29,8 @@ class Icon:
         if self.flash_counter > self.flash_threshold:
             self.flash_counter = 0
             self.visible = False if self.visible else True
+
+    def draw(self, display):
+
+        if self.visible:
+            display.blit(self.image, (self.rect.x, self.rect.y))
