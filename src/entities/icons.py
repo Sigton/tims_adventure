d952a845a196f2634b561dcae3d01da85b9e96f3
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-12-19 16:34:57 +0000

    Created Icon parent class

--- src/entities/icons.py
+++ src/entities/icons.py
@@ -6,3 +6,11 @@ small icons that appear about the place
 """
 
 
+class Icon:
+
+    def __init__(self, image, flash):
+
+        self.image = image
+        self.rect = self.image.get_rect()
+
+        self.flash = flash
