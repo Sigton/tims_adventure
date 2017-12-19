89062e5c38906e33fb9c853ac16ff9963b4cdd95
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-12-19 16:37:18 +0000

    Created Icon.update

--- src/entities/icons.py
+++ src/entities/icons.py
@@ -8,9 +8,21 @@ small icons that appear about the place
 
 class Icon:
 
-    def __init__(self, image, flash):
+    def __init__(self, image, flash=False, flash_threshold=0):
 
         self.image = image
         self.rect = self.image.get_rect()
 
         self.flash = flash
+        self.flash_threshold = flash_threshold
+        self.flash_counter = 0
+
+        self.visible = True
+
+    def update(self):
+
+        self.flash_counter += 1
+
+        if self.flash_counter > self.flash_threshold:
+            self.flash_counter = 0
+            self.visible = False if self.visible else True
