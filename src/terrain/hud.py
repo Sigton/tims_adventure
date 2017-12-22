dccbf7c9245abe0cc1049e0f852a4f6a80e0573c
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-12-22 23:16:33 +0000

    Fixing the labels

--- src/terrain/hud.py
+++ src/terrain/hud.py
@@ -20,7 +20,8 @@ class HUD:
         self.health_bars = [gui_components.ProgressBar(9, 27+36*n, 182, 4,
                                                        (constants.HEALTH_BAR_RED, constants.HEALTH_BAR_GREEN))
                             for n in range(5)]
-        self.bean_labels = [gui_components.Label(9, 3+40*n, "{} Bean".format(self.player.beans[n].bean),
+        self.bean_labels = [gui_components.Label(9, 3+40*n, "{}{} Bean".format(
+            self.player.beans[n].bean[0].upper(), self.player.beans[n].bean[1:]),
                                                  False, 20, constants.BLACK)
                             for n in range(5)]
 
@@ -55,9 +56,11 @@ class HUD:
             if panel_idx == 0:
                 stat_panel.rect.top = 5
                 self.health_bars[panel_idx].rect.top = 27
+                self.bean_labels[panel_idx].rect.top = 3
             else:
                 stat_panel.rect.top = self.bean_stats[panel_idx-1].rect.bottom + 5
                 self.health_bars[panel_idx].rect.top = self.bean_stats[panel_idx-1].rect.bottom + 27
+                self.bean_labels[panel_idx].rect.top = self.bean_stats[panel_idx - 1].rect.bottom + 3
 
             panel_idx += 1
 
