e0418410b11dd4fb6036aed348b687448f77fcfc
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-12-22 17:57:21 +0000

    Working on active bean stats

--- src/terrain/hud.py
+++ src/terrain/hud.py
@@ -13,14 +13,16 @@ class HUD:
     def __init__(self):
 
         self.background = gui_components.Fill(0, 0, 200, 209, constants.GUI_BACKING)
-
         self.bean_stats = [gui_components.Fill(5, 5+35*n, 190, 30, constants.GUI_FILL) for n in range(5)]
 
         self.components = [self.background] + self.bean_stats
 
+        self.active_bean_stat = 0
+
     def update(self):
 
-        pass
+        if self.bean_stats[self.active_bean_stat].rect.height == 30:
+            pass
 
     def draw(self, display):
 
