eed072f79079a9919ddc80716bfd1ace99a89a5a
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-12-22 17:02:38 +0000

    Created HUD.py

---
+++ src/terrain/hud.py
@@ -0,0 +1,21 @@
+"""
+hud.py
+
+This file manages the
+games heads-up display
+"""
+
+
+class HUD:
+
+    def __init__(self):
+
+        pass
+
+    def update(self):
+
+        pass
+
+    def draw(self, display):
+
+        pass
