11f0d6f13c91191ecf86d99a3da130a4aceb867b
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-12-22 12:52:43 +0000

    Working on DuelPlayer class

--- src/duel/duel_players.py
+++ src/duel/duel_players.py
@@ -1,3 +1,5 @@
+import pygame
+
 """
 duel_players.py
 
@@ -8,6 +10,12 @@ to the player and enemy in duelling
 
 class DuelPlayer:
 
-    def __init__(self):
+    def __init__(self, player, side):
 
-        pass
+        self.meta = player.meta
+        self.side = side
+
+        self.energy_max = self.meta.energy
+        self.energy = self.energy_max
+
+        self.image = pygame.transform.scale(self.meta.images[self.side], (300, 300) if self.side=="R" else (230, 230))
