b8a631abbed30e55c1883a5da2a294f2060e8b9c
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-12-22 17:10:30 +0000

    Created Fill gui component

--- src/etc/gui_components.py
+++ src/etc/gui_components.py
@@ -126,3 +126,15 @@ class Label:
     def draw(self, display):
 
         display.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Fill:
+
+    def __init__(self, x, y, width, height, color):
+
+        self.image = pygame.Surface([width, height]).convert()
+        self.image.fill(color)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
