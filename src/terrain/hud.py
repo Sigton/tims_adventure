473f7160fe1c18a649aba436779a42b45db826a7
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-12-22 17:56:20 +0000

    Adding more to the gui

--- src/terrain/hud.py
+++ src/terrain/hud.py
@@ -14,7 +14,9 @@ class HUD:
 
         self.background = gui_components.Fill(0, 0, 200, 209, constants.GUI_BACKING)
 
-        self.components = [self.background]
+        self.bean_stats = [gui_components.Fill(5, 5+35*n, 190, 30, constants.GUI_FILL) for n in range(5)]
+
+        self.components = [self.background] + self.bean_stats
 
     def update(self):
 
