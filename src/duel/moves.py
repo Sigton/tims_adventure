957876631af72c177dee438fbf6fc2336237d375
Jamie Buttenshaw <sigton.scratch@gmail.com>
2017-12-24 22:32:05 +0000

    Created new move

--- src/duel/moves.py
+++ src/duel/moves.py
@@ -14,5 +14,7 @@ moves = [
     {"name": "Burn", "str_mod": 1.4, "effects":
         """self.particle_engine.create_particle_spread('fire', 30, {}, {}, 130, 25, 20, 25, 5)
 self.master.sound_engine.queue_sound((self.master.sound_engine.burn, 0))""",
-     "energy": 15, "xp": 15}
+     "energy": 15, "xp": 15},
+    {"name": "Freeze", "str_mod": 1.5, "effects":
+        """self.particle_engine.create_particle_spread('fire', 30, {}, {}, 130, 25, 20, 25, 5)"""}
 ]
