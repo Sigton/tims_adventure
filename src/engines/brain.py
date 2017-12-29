db4c9b86ee1a626a113a9d48653479b437722c87
Sigton <sigton.scratch@gmail.com>
2017-12-29 17:51:53 +0000

    Added expected outcome to test scenarios

--- src/engines/brain.py
+++ src/engines/brain.py
@@ -13,13 +13,14 @@ in the duelling.
 weights_preset = [
     [2, 3, 3, 0.5, 3, 1, 1, 3, 1, 0.5, 1.5, 0.5],
     [2, 3, 0.5, 3, 3, 1, 1, 3, 1, 1.5, 0.5, 0.5],
-    [0.5, -1.5, 1, 1, -0.5, 3, 3, -1.5, 6, 1.5, 1.5, 3],
+    [0.5, -1.5, 1, 1, -0.5, 3, 3, -1.5, 2, 1.5, 1.5, 3],
     [-1, -2, 1, 1, -0.5, 0.5, 0.5, -4, 3, 1, 1, 3]
 ]
 
 scenarios = [
-    [100, 0, 14, 10, 100, 0, 0, 0, 0, 15, 5, 0],
-    [10, -40, 14, 10, 20, 1, 2, -1, 90, 15, 5, 80]
+    ([100, 0, 14, 10, 100, 0, 0, 0, 0, 15, 5, 0], 0),
+    ([10, -40, 14, 10, 20, 1, 2, -1, 90, 15, 5, 80], 2),
+    ([4, -26, 20, 7, 5, 0, 0, -2, 96, 20, 5, 95], 3)
 ]
 
 
@@ -36,6 +37,7 @@ class Brain:
     def get_output(self, in_data):
 
         out_data = [sum([in_data[n]*weights_preset[x][n] for n in range(self.inputs)]) for x in range(self.outputs)]
+        print(out_data)
         return out_data.index(max(out_data))
 
 
@@ -46,4 +48,4 @@ if __name__ == "__main__":
     n = 0
     for s in scenarios:
         n += 1
-        print("Scenario {}: {}".format(n, test_brain.get_output(s)))
+        print("Scenario {}: {}".format(n, test_brain.get_output(s[0])))
