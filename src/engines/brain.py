1962af8b6c00f55d709ed05c2a217eb89d3f02b2
Sigton <sigton.scratch@gmail.com>
2017-12-29 19:21:32 +0000

    brain now imports scenarios from training data

--- src/engines/brain.py
+++ src/engines/brain.py
@@ -1,5 +1,4 @@
-import random
-import math
+import json
 
 """
 brain.py
@@ -16,12 +15,8 @@ weights_preset = [
     [0.5, -1.5, 1, 1, -0.5, 3, 3, -1.5, 2, 1.5, 1.5, 3],
     [-1, -2, 1, 1, -0.5, 0.5, 0.5, -4, 3, 1, 1, 3]
 ]
-
-scenarios = [
-    ([100, 0, 14, 10, 100, 0, 0, 0, 0, 15, 5, 0], 0),
-    ([10, -40, 14, 10, 20, 1, 2, -1, 90, 15, 5, 80], 2),
-    ([4, -26, 20, 7, 5, 0, 0, -2, 96, 20, 5, 95], 3)
-]
+with open("src/saves/training_data.json", 'r') as infile:
+    scenarios = json.load(infile)["scenarios"]
 
 
 class Brain:
@@ -37,7 +32,6 @@ class Brain:
     def get_output(self, in_data):
 
         out_data = [sum([in_data[n]*weights_preset[x][n] for n in range(self.inputs)]) for x in range(self.outputs)]
-        print(out_data)
         return out_data.index(max(out_data))
 
 
