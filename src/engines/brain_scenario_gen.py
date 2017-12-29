2e887d0b0e18631225c7777b62b8568748aab6ae
Sigton <sigton.scratch@gmail.com>
2017-12-29 18:11:38 +0000

    Created a scenario generator

---
+++ src/engines/brain_scenario_gen.py
@@ -0,0 +1,39 @@
+import randomgit
+import json
+
+scenarios = []
+
+for n in range(int(input("How many scenarios:")))
+    maxHP = random.randint(100,250)
+    HP = maxHP * random.uniform(0, 1)
+    hp_lost = maxHP - HP
+
+    playerHP = random.randint(100,250)*random.uniform(0,1)
+
+    hp_diff = HP - playerHP
+
+    m_attack_hp = random.randint(5,30)
+    a_attack_hp = random.randint(5,30)
+
+    max_energy = 100
+    energy = max_energy*random.uniform(0,1)
+    energy_lost = max_energy-energy
+
+    no_hp_items = random.choice([0,0,0,0,1,1,1,2,2,3])
+    no_en_items = random.choice([0,0,0,0,1,1,1,2,2,3])
+
+    level = random.randint(1,5)
+    player_level = random.randint(1,5)
+
+    level_diff = level-player_level
+
+    m_attack_en = m_attack_hp*random.uniform(0.5,1.5)
+    a_attack_en = e_attack_hp*random.uniform(0.5,1.5)
+
+    s=[HP, hp_diff, m_attack_hp, a_attack_hp, energy, no_hp_items, no_en_items, level_diff,
+       hp_lost, m_attack_en, a_attack_en, energy_lost]
+    print(s)
+    expected_outcome = input("Expected outcome:")
+
+    scenarios += [(s, expected_outcome)]
+
