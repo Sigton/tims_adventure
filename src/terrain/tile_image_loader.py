9cee5ce99e9e0ae044e0773fef890f2b313834aa
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-01-09 16:47:55 +0000

    Added a new tile

--- src/terrain/tile_image_loader.py
+++ src/terrain/tile_image_loader.py
@@ -73,6 +73,7 @@ shore_7 = {"template": (96, 288, 48, 48),
 shore_8 = {"template": (144, 288, 48, 48),
            "material": 0}
 bush = (384, 192, 96, 48)
+grass = (480, 0, 48, 48)
 
 
 # Indexing this array by a tiles' ID will
@@ -88,7 +89,7 @@ tiles = [generic_ground, blue_ground, path_1,
          wall_11, wall_12, choc_river,
          tree, shore_1, shore_2,
          shore_3, shore_4, shore_5, shore_6,
-         shore_7, shore_8, bush]
+         shore_7, shore_8, bush, grass]
 images = {}
 
 sprite_sheet = None
