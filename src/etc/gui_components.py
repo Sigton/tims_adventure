d1c3c01642bc9062a2d76bc924304252735d5d7f
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-01-13 22:36:48 +0000

    Created an image class

--- src/etc/gui_components.py
+++ src/etc/gui_components.py
@@ -184,3 +184,14 @@ class Fade:
     def set_opacity(self, opacity):
 
         self.opacity = opacity
+
+
+class Image:
+
+    def __init__(self, image_path, x=0, y=0):
+
+        self.image = pygame.image.load(image_path).convert()
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
