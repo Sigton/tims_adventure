847a71c34de8c0b5f17087c9a9a55fc224f616fc
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-01-13 18:02:27 +0000

    Created MainMenu class

--- src/engines/menu.py
+++ src/engines/menu.py
@@ -4,3 +4,18 @@ menu.py
 This file loads and defines
 the different components of the menu.
 """
+
+
+class MainMenu:
+
+    def __init__(self):
+
+        pass
+
+    def update(self):
+
+        pass
+
+    def draw(self):
+
+        pass
