b246d7ecbad51354b8842f6174337f93bec87ec0
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-01-14 09:29:23 +0000

    Defined SpriteSheetData

--- src/menu/menu_image_loader.py
+++ src/menu/menu_image_loader.py
@@ -4,3 +4,24 @@ menu_image_loader.py
 This file loads the different images
 used in the main menu
 """
+
+
+class SpriteSheetData:
+
+    play_button = [
+        (0, 0, 212, 92),
+        (212, 0, 212, 92),
+        (424, 0, 212, 92)
+    ]
+
+    options_button = [
+        (0, 92, 212, 92),
+        (212, 92, 212, 92),
+        (424, 92, 212, 92)
+    ]
+
+    quit_button = [
+        (0, 184, 212, 92),
+        (212, 184, 212, 92),
+        (424, 92, 212, 92)
+    ]
