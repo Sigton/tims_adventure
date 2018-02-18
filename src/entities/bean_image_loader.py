0386ed8767a97067d88d9025958b22e2a1acc39f
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-02-18 20:13:52 +0000

    Added hedgehog bean and pickle bean

--- src/entities/bean_image_loader.py
+++ src/entities/bean_image_loader.py
@@ -11,20 +11,22 @@ sprite_sheet = None
 
 chili = lambda: sprite_sheet.get_image(0, 0, 40, 40)
 cool = lambda: sprite_sheet.get_image(40, 0, 40, 40)
-poison = lambda: sprite_sheet.get_image(80, 0, 40, 40)
+pickle = lambda: sprite_sheet.get_image(80, 0, 40, 40)
 strawberry = lambda: sprite_sheet.get_image(120, 0, 40, 40)
 lemon = lambda: sprite_sheet.get_image(160, 0, 40, 40)
 rainbow = lambda: sprite_sheet.get_image(200, 0, 40, 40)
 unicorn = lambda: sprite_sheet.get_image(240, 0, 40, 40)
+hedgehog = lambda: sprite_sheet.get_image(280, 0, 40, 40)
 
 beans = {
     "chili": chili,
     "cool": cool,
-    "poison": poison,
+    "pickle": pickle,
     "strawberry": strawberry,
     "lemon": lemon,
     "rainbow": rainbow,
-    "unicorn": unicorn
+    "unicorn": unicorn,
+    "hedgehog": hedgehog
 }
 
 
--- src/entities/entity_meta.py
+++ src/entities/entity_meta.py
@@ -9,9 +9,10 @@ their max hp, moves, starting damage.
 entity_data = {
     "chili": {"max_hp": 100, "moves": [1, 0], "attack": 10, "energy": 100},
     "cool": {"max_hp": 100, "moves": [0, 2], "attack": 10, "energy": 100},
-    "poison": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
+    "pickle": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
     "strawberry": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
     "lemon": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
     "rainbow": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
-    "unicorn": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100}
+    "unicorn": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
+    "hedgehog": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100}
 }
