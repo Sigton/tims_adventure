3e8fcff94b5fce88307ba0d3e91f1bd5ebf91bfa
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-02-18 11:55:03 +0000

    Working on the beans

--- src/entities/bean_image_loader.py
+++ src/entities/bean_image_loader.py
@@ -9,17 +9,17 @@ the bean images
 
 sprite_sheet = None
 
-red = lambda: sprite_sheet.get_image(0, 0, 40, 40)
+chili = lambda: sprite_sheet.get_image(0, 0, 40, 40)
 cool = lambda: sprite_sheet.get_image(40, 0, 40, 40)
 green = lambda: sprite_sheet.get_image(80, 0, 40, 40)
-pink = lambda: sprite_sheet.get_image(120, 0, 40, 40)
+strawberry = lambda: sprite_sheet.get_image(120, 0, 40, 40)
 yellow = lambda: sprite_sheet.get_image(160, 0, 40, 40)
 
 beans = {
-    "red": red,
+    "chili": chili,
     "cool": cool,
     "green": green,
-    "pink": pink,
+    "strawberry": strawberry,
     "yellow": yellow
 }
 
