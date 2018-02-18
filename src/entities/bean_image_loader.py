ec6558057833a5b761ebb8b897e71bb515190cb4
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-02-18 12:15:09 +0000

    Added poison bean

--- src/entities/bean_image_loader.py
+++ src/entities/bean_image_loader.py
@@ -11,14 +11,14 @@ sprite_sheet = None
 
 chili = lambda: sprite_sheet.get_image(0, 0, 40, 40)
 cool = lambda: sprite_sheet.get_image(40, 0, 40, 40)
-green = lambda: sprite_sheet.get_image(80, 0, 40, 40)
+poison = lambda: sprite_sheet.get_image(80, 0, 40, 40)
 strawberry = lambda: sprite_sheet.get_image(120, 0, 40, 40)
 lemon = lambda: sprite_sheet.get_image(160, 0, 40, 40)
 
 beans = {
     "chili": chili,
     "cool": cool,
-    "green": green,
+    "poison": poison,
     "strawberry": strawberry,
     "lemon": lemon
 }
--- src/entities/entity_meta.py
+++ src/entities/entity_meta.py
@@ -9,7 +9,7 @@ their max hp, moves, starting damage.
 entity_data = {
     "chili": {"max_hp": 100, "moves": [1, 0], "attack": 10, "energy": 100},
     "cool": {"max_hp": 100, "moves": [0, 2], "attack": 10, "energy": 100},
-    "green": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
+    "poison": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
     "strawberry": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
     "lemon": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100}
 }
