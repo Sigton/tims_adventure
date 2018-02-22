51c9dda5e0cd7bef58b8b5e3a6949ed6abf6b628
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-02-22 16:28:11 +0000

    Created dialogue.py

---
+++ src/engines/dialogue.py
@@ -0,0 +1,20 @@
+"""
+dialogue.py
+
+Handles interactions between characters.
+"""
+
+
+class DialogueController:
+
+    def __init__(self):
+
+        pass
+
+    def update(self):
+
+        pass
+
+    def draw(self):
+
+        pass
