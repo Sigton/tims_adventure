16e2170607ef18c2c022c6b6edf246b1aa7235b7
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-02-25 08:14:53 +0000

    Disabled randomly generating beans while testing player saving

--- src/engines/save.py
+++ src/engines/save.py
@@ -73,7 +73,7 @@ class SaveEngine:
         with open(os.path.join(save_path, "meta.json"), "w") as outfile:
             json.dump(entity_data, outfile)
 
-        gen_random_entities(save_path)
+        # gen_random_entities(save_path)
 
         os.remove(os.path.join(save_path, "decs.json"))
         os.remove(os.path.join(save_path, "entities.json"))
