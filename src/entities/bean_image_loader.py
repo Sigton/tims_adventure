544e4daa1945101609cfcbafbb349af357bcbf35
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-02-27 20:05:04 +0000

    Reworking things a little bit

--- src/entities/bean_image_loader.py
+++ src/entities/bean_image_loader.py
@@ -9,42 +9,41 @@ the bean images
 
 sprite_sheet = None
 
-chili = lambda: sprite_sheet.get_image(0, 0, 40, 40)
-cool = lambda: sprite_sheet.get_image(40, 0, 40, 40)
-pickle = lambda: sprite_sheet.get_image(80, 0, 40, 40)
-strawberry = lambda: sprite_sheet.get_image(120, 0, 40, 40)
-lemon = lambda: sprite_sheet.get_image(160, 0, 40, 40)
-rainbow = lambda: sprite_sheet.get_image(200, 0, 40, 40)
-unicorn = lambda: sprite_sheet.get_image(240, 0, 40, 40)
-hedgehog = lambda: sprite_sheet.get_image(280, 0, 40, 40)
-poison = lambda: sprite_sheet.get_image(320, 0, 40, 40)
-carrot = lambda: sprite_sheet.get_image(0, 40, 42, 46)
-rabbit = lambda: sprite_sheet.get_image(360, 0, 40, 40)
-what = lambda: sprite_sheet.get_image(42, 40, 40, 40)
-chicken = lambda: sprite_sheet.get_image(82, 40, 40, 40)
+chili = (0, 0, 40, 40)
+cool = (40, 0, 40, 40)
+pickle = (80, 0, 40, 40)
+strawberry = (120, 0, 40, 40)
+lemon = (160, 0, 40, 40)
+rainbow = (200, 0, 40, 40)
+unicorn = (240, 0, 40, 40)
+hedgehog = (280, 0, 40, 40)
+poison = (320, 0, 40, 40)
+carrot = (0, 40, 42, 46)
+rabbit = (360, 0, 40, 40)
+what = (42, 40, 40, 40)
+chicken = (82, 40, 40, 40)
 
-beans = {
-    "chili": chili,
-    "cool": cool,
-    "pickle": pickle,
-    "strawberry": strawberry,
-    "lemon": lemon,
-    "rainbow": rainbow,
-    "unicorn": unicorn,
-    "hedgehog": hedgehog,
-    "poison": poison,
-    "carrot": carrot,
-    "rabbit": rabbit,
-    "what": what,
-    "chicken": chicken
-}
+beans = {}
 
 
 def load_sprite_sheet():
 
-    global sprite_sheet
+    global sprite_sheet, beans
     sprite_sheet = spritesheet.SpriteSheet("src/resources/beans.png")
 
-def create_images(bean):
+    beans = {
+        "chili": chili,
+        "cool": cool,
+        "pickle": pickle,
+        "strawberry": strawberry,
+        "lemon": lemon,
+        "rainbow": rainbow,
+        "unicorn": unicorn,
+        "hedgehog": hedgehog,
+        "poison": poison,
+        "carrot": carrot,
+        "rabbit": rabbit,
+        "what": what,
+        "chicken": chicken
+    }
 
-    pass
