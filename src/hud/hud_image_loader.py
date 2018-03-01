6e9f212fda5fbce4ebb7bd7724c94ab9431ed207
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-03-01 11:27:05 +0000

    Populated spritesheet data

--- src/hud/hud_image_loader.py
+++ src/hud/hud_image_loader.py
@@ -9,15 +9,22 @@ Loads various images that are used in the hud widgets
 
 sprite_sheet_data = {
     "inventory_button": [
-
+        (0, 56, 60, 60),
+        (0, 116, 60, 60),
+        (0, 176, 60, 60)
     ],
     "journal_button": [
-
+        (60, 56, 60, 60),
+        (60, 116, 60, 60),
+        (60, 176, 60, 60)
     ],
     "map_button": [
-
+        (120, 56, 60, 60),
+        (120, 116, 60, 60),
+        (120, 176, 60, 60)
     ]
 }
+
 
 def load_images(image):
 
