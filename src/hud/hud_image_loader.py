8c76899f710cfa6d43cade75102f35bc17dc40ef
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-03-01 11:25:14 +0000

    Updated hud_image_loader

--- src/hud/hud_image_loader.py
+++ src/hud/hud_image_loader.py
@@ -5,3 +5,20 @@ hud_image_loader.py
 
 Loads various images that are used in the hud widgets
 """
+
+
+sprite_sheet_data = {
+    "inventory_button": [
+
+    ],
+    "journal_button": [
+
+    ],
+    "map_button": [
+
+    ]
+}
+
+def load_images(image):
+
+    pass
