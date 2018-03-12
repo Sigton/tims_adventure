5ccd435cc87e26d38529eb1bd66a348b43261e25
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-03-12 21:11:43 +0000

    Adding in a new dialogue scene

--- src/etc/dialogue_scenes.py
+++ src/etc/dialogue_scenes.py
@@ -6,15 +6,18 @@ conversations between the beans.
 """
 
 scenes = {
-            "villager1": [
-                ("What's happening here?", 0),
-                ("They're coming!", 1),
-                ("Who is?", 0),
-                ("We have to run!", 1)
-            ],
-            "villager2": [
-                ("We can't stay here anymore.", 1),
-                ("Why not?", 0),
-                ("They'll be here soon, we need to leave now.", 1)
-            ]
-        }
+    "villager1": [
+        ("What's happening here?", 0),
+        ("They're coming!", 1),
+        ("Who is?", 0),
+        ("We have to run!", 1)
+    ],
+    "villager2": [
+        ("We can't stay here anymore.", 1),
+        ("Why not?", 0),
+        ("They'll be here soon, we need to leave now.", 1)
+    ],
+    "villager3": [
+        ("Strange things are happening here.", 1)
+    ]
+}
