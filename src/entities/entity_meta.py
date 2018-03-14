ef7ba89690b5f5c71f204c95a917f3edeaaed4fa
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-03-14 18:06:43 +0000

    Added display names to the bean meta

--- src/entities/entity_meta.py
+++ src/entities/entity_meta.py
@@ -7,21 +7,21 @@ their max hp, moves, starting damage.
 """
 
 entity_data = {
-    "chili": {"max_hp": 100, "moves": [1, 0], "attack": 10, "energy": 100},
-    "cool": {"max_hp": 100, "moves": [0, 2], "attack": 10, "energy": 100},
-    "pickle": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
-    "strawberry": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
-    "lemon": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
-    "rainbow": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
-    "unicorn": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
-    "hedgehog": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
-    "poison": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
-    "carrot": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
-    "rabbit": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
-    "what": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
-    "chicken": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
-    "wizard": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100},
-    "old_villager": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100}
+    "chili": {"max_hp": 100, "moves": [1, 0], "attack": 10, "energy": 100, "display_name": "Chili"},
+    "cool": {"max_hp": 100, "moves": [0, 2], "attack": 10, "energy": 100, "display_name": "Cool"},
+    "pickle": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100, "display_name": "Pickle"},
+    "strawberry": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100, "display_name": "Strawberry"},
+    "lemon": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100, "display_name": "Lemon"},
+    "rainbow": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100, "display_name": "Rainbow"},
+    "unicorn": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100, "display_name": "Unicorn"},
+    "hedgehog": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100, "display_name": "Hedgehog"},
+    "poison": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100, "display_name": "Poison"},
+    "carrot": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100, "display_name": "Carrot"},
+    "rabbit": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100, "display_name": "Rabbit"},
+    "what": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100, "display_name": "What"},
+    "chicken": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100, "display_name": "Chicken"},
+    "wizard": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100, "display_name": "Wizard"},
+    "old_villager": {"max_hp": 100, "moves": [0, 1], "attack": 10, "energy": 100, "display_name": "Old"}
 }
 
 item_effects = {
