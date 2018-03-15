12a005413a5c531cefcfafccd0a8288f19d9ef79
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-03-15 16:28:41 +0000

    Fixed formatting and a syntax error

--- src/etc/dialogue_scenes.py
+++ src/etc/dialogue_scenes.py
@@ -26,7 +26,7 @@ scenes = {
     "villager4": [
         ("Can you help me?", 0),
         ("I'm not the one to be asking. There's an elder in the town, try him.", 1)
-    ]
+    ],
     "old_man": [
         ("You came to see me?", 1),
         ("Yes, what is happening here?", 0),
@@ -34,13 +34,16 @@ scenes = {
         ("It all started when they arrived in the land.", 1),
         ("Who?", 0),
         ("The formidable, corrupted Jelly Monster. It came to our lands, and brought its negativity with it.", 1),
-        ("The three wizard guardians of the land tried to stop the monster, but its negativity was too strong for even the wizard's magical power.", 1),
+        ("""The three wizard guardians of the land tried to stop the monster, but its negativity was too strong for 
+even the wizard's magical power.""", 1),
         ("""Before the wizard's themselves were dragged into darkness, they united their 
 power and built gates between the sections of the land.""", 1),
         ("Despite the wizard's efforts, there is still little hope for the land. There is evil everywhere.", 1),
-        ("This village is one of the last standing. All of the villagers are worried; there is not long left until the evil beans reach here.", 1),
+        ("""This village is one of the last standing. All of the villagers are worried; there is not long left until 
+the evil beans reach here.""", 1),
         ("What will happen once they reach the village?", 0),
-        ("These villagers will become corrupted. Their minds cast into darkness, and they will join the ranks of the jelly monsters army.", 1),
+        ("""These villagers will become corrupted. Their minds cast into darkness, and they will join the ranks of the 
+jelly monsters army.""", 1),
         ("We need a brave, brave warrior to stop the jelly monster and its evil beans.", 1)
     ]
 }
