d344e751e74944a84401f714471dbb3708a75fd3
Jamie <Sigton@users.noreply.github.com>
2018-03-15 13:57:03 +0000

    Working on the script

--- src/etc/dialogue_scenes.py
+++ src/etc/dialogue_scenes.py
@@ -29,11 +29,14 @@ scenes = {
         ("Dark things are happening. Dark, dark things...", 1),
         ("It all started when they arrived in the land.", 1),
         ("Who?", 0),
-        ("The formidable, corrupted Jelly Monster. It came to our lands, and brought his negativity with it.", 1),
-        ("The three wizard guardians of the land tried to stop the monster, but his negativity was too strong.", 1),
-        ("""Before the wizards themselves were dragged into darkness, they united their 
+        ("The formidable, corrupted Jelly Monster. It came to our lands, and brought its negativity with it.", 1),
+        ("The three wizard guardians of the land tried to stop the monster, but its negativity was too strong for even the wizard's magical power.", 1),
+        ("""Before the wizard's themselves were dragged into darkness, they united their 
 power and built gates between the sections of the land.""", 1),
-        ("Despite these wizards efforts, there is still little hope for the land. There is evil everywhere.", 1),
-        ("This village is one of the last standing. All of the villagers are worried; there is not long left until the evil beans reach here.", 1)
+        ("Despite the wizard's efforts, there is still little hope for the land. There is evil everywhere.", 1),
+        ("This village is one of the last standing. All of the villagers are worried; there is not long left until the evil beans reach here.", 1),
+        ("What will happen once they reach the village?", 0),
+        ("These villagers will become corrupted. Their minds cast into darkness, and they will join the ranks of the jelly monsters army.", 1),
+        ("We need a brave, brave warrior to stop the jelly monster and its evil beans.", 1)
     ]
 }
