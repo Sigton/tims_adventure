dcf6fbe098dddc0d963274669732f69782d8462f
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-03-16 19:19:08 +0000

    Fixed bug

--- src/engines/story_tracker.py
+++ src/engines/story_tracker.py
@@ -58,7 +58,7 @@ class StoryTracker:
     def add_quest(self, quest):
 
         if quest not in self.quests.keys():
-            self.quests[quest] = quests[quest]
+            self.quests[quest] = self.quests[quest]
 
     def remove_quest(self, quest):
 
