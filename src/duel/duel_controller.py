6946db9e524d4157f9d3761f20189d039d878e13
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-03-28 17:25:56 +0100

    Implemented quest completion through duels

--- src/duel/duel_controller.py
+++ src/duel/duel_controller.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.locals import *
 
 from src.duel.duel_gui import load_images
-from src.etc import gui_components, constants
+from src.etc import gui_components, constants, story_data
 from src.duel.duel_players import DuelPlayer
 from src.duel.moves import moves
 from src.engines import brain
@@ -162,6 +162,21 @@ class DuelController:
 
         self.text.append(self.player_name_label)
         self.text.append(self.enemy_name_label)
+
+        quests_to_follow = []
+
+        for path in story_data.quest_path.items():
+            if enemy.meta.id in path[1] and not self.master.story_tracker.is_complete(path[0]):
+                quests_to_follow += [path[0]]
+
+        [self.master.story_tracker.follow_path(quest) for quest in quests_to_follow]
+
+        for quest in self.master.story_tracker.quests.items():
+            for criteria in story_data.completion_criteria[quest[0]]:
+                if criteria.split("/") == ["duel", enemy.meta.id]:
+                    quests_to_follow += [quest[0]]
+
+        [self.master.story_tracker.follow_path(quest) for quest in quests_to_follow]
 
         self.reset()
 
