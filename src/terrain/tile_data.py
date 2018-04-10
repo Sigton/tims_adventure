9b3fc079c088fff6af7a08ccb4e5350a2d4faa37
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-04-10 14:55:40 +0100

    Adding a variant of the boardwalk poles

--- src/terrain/tile_data.py
+++ src/terrain/tile_data.py
@@ -81,5 +81,6 @@ tile_colors = {15724527: "0000",  # Generic ground
                16252672: "0074",  # Boardwalk 1
                13028352: "0075",  # Boardwalk 2
                8398970: "0076",  # Spruce Tree
-               9804032: "0077",  # Boardwalk poles
+               9804032: "0077",  # Boardwalk Poles 1
+               6514176: "0078",  # Boardwalk Poles 2
                }
