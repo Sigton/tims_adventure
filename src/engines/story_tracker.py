054e448e465b3c69261f26e3d9fc909a76f09d6f
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-04-13 18:57:09 +0100

    Populated quest update

--- src/engines/story_tracker.py
+++ src/engines/story_tracker.py
@@ -159,4 +159,6 @@ class StoryTracker:
 
     def update_dan(self):
 
-        pass
+        self.master.chunk_controller.locate_entity(14).meta.interaction = \
+            """self.master.dialogue_controller.start_scene(self.player.beans[0], self.enemy_to_duel, 'fisherman_duel2', 
+'self.master.duel_controller.begin_duel(self.player_ref, self.other_bean_ref)', 1);self.master.switch_to(3)"""
