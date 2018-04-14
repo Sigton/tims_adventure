4d48d8a4dc2edee7da64ef302aabb0c8d7bb3092
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-04-14 20:47:05 +0100

    Balanced energy usage/xp gain

--- src/duel/moves.py
+++ src/duel/moves.py
@@ -10,7 +10,7 @@ a player can make.
 moves = [
     {"name": "Tackle", "str_mod": 1, "effects":
         "self.start_shake(18, 10, 10, {}1);self.master.sound_engine.queue_sound(('punch', 0))",
-     "energy": 5, "xp": 5},
+     "energy": 6, "xp": 6},
     {"name": "Burn", "str_mod": 1.4, "effects":
         """self.particle_engine.create_particle_spread('fire', 30, {}, {}, 130, 25, 20, 25, 5)
 self.master.sound_engine.queue_sound(('burn', 0))""",
@@ -20,23 +20,23 @@ self.master.sound_engine.queue_sound(('burn', 0))""",
      "energy": 20, "xp": 20},
     {"name": "Chili Chuck", "str_mod": 0.7, "effects":
         """self.particle_engine.create_particle_spread('chili', 45, {}, {}, 150, 15, 10, 10, 10)""",
-     "energy": 4, "xp": 4},
+     "energy": 5, "xp": 5},
     {"name": "Punch", "str_mod": 1.2, "effects":
         """self.start_shake(18, 10, 10, {}1);self.master.sound_engine.queue_sound(('punch', 0))""",
      "energy": 8, "xp": 8},
     {"name": "Snowball", "str_mod": 0.6, "effects":
         """self.start_shake(18, 10, 10, {}1);self.master.sound_engine.queue_sound(('punch', 0))""",
-     "energy": 8, "xp": 8},
+     "energy": 4, "xp": 4},
     {"name": "Feathers", "str_mod": 0.9, "effects":
         """self.start_shake(18, 10, 10, {}1);self.master.sound_engine.queue_sound(('punch', 0))""",
-     "energy": 8, "xp": 8},
+     "energy": 6, "xp": 6},
     {"name": "Sneak Attack", "str_mod": 1.3, "effects":
         """self.start_shake(18, 10, 10, {}1);self.master.sound_engine.queue_sound(('punch', 0))""",
-     "energy": 8, "xp": 8},
+     "energy": 13, "xp": 13},
     {"name": "Poison Spit", "str_mod": 1.7, "effects":
         """self.start_shake(18, 10, 10, {}1);self.master.sound_engine.queue_sound(('punch', 0))""",
-     "energy": 8, "xp": 8},
+     "energy": 23, "xp": 23},
     {"name": "Citrus Spray", "str_mod": 1.2, "effects":
         """self.start_shake(18, 10, 10, {}1);self.master.sound_engine.queue_sound(('punch', 0))""",
-     "energy": 8, "xp": 8},
+     "energy": 10, "xp": 10},
 ]
