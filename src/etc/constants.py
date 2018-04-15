864ffe62db5824fc73df0b2960a77a22ad81399f
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-04-15 11:17:25 +0100

    Fixed item spawning weighting

--- src/etc/constants.py
+++ src/etc/constants.py
@@ -271,6 +271,6 @@ quest_images = {
     "defend_wizard": (32, 268, 32, 32)
 }
 
-item_rarity_weights = [0, 24, 8, 5, 3]
+item_rarity_weights = [0, 24, 5, 8, 3]
 item_rarity_matrix = [int(x) for x in sum([list(x * y)
                                            for x, y in zip(["0", "1", "2", "3", "4"], item_rarity_weights)], [])]
